3a3e3f4958835ccadc71dc393e247c370b877139
yasojan <yasinzahir98@gmail.com>
2025-12-14 15:16:00 -0800

    Create total variable to include sum

--- curriculum-main/foundations/javascript_basics/javascript-exercises-main/foundations/09_sumAll/sumAll.js
+++ curriculum-main/foundations/javascript_basics/javascript-exercises-main/foundations/09_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  let total = 0;
+  for(let i = num1; i<=num2; i++){
+    total+=i;
+  }
+  return total;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+console.log(sumAll(1, 4)); // returns the sum of 1 + 2 + 3 + 4 which is 10)
