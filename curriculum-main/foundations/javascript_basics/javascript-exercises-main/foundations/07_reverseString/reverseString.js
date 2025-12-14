f5b8fcefcca6f70e32e05a2fd6d27bc99c469da7
yasojan <yasinzahir98@gmail.com>
2025-12-14 15:08:22 -0800

    Edit .js file to reverse a string

--- curriculum-main/foundations/javascript_basics/javascript-exercises-main/foundations/07_reverseString/reverseString.js
+++ curriculum-main/foundations/javascript_basics/javascript-exercises-main/foundations/07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let text = str.split("");
+  return text.reverse().join("");
 };
 
+console.log(reverseString('hello there')); // returns 'ereht olleh'
 // Do not edit below this line
 module.exports = reverseString;
