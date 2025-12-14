03796017337353d394b18dd2b03722cf436f1449
yasojan <yasinzahir98@gmail.com>
2025-12-14 08:20:49 -0800

    Solve coding challenge #4

--- curriculum-main/foundations/javascript_basics/loops_and_arrays/03.js
+++ curriculum-main/foundations/javascript_basics/loops_and_arrays/03.js
@@ -20,7 +20,7 @@ alert( arr ); // [3, 1]
 function filterRangeInPlace(arr, num1, num2){
   for(let i =0; i<arr.length; i++){
     if(arr[i] < num1 || arr[i] > num2){
-      delete arr[i];
+      arr.splice(i, 1);
     }
   }
   return arr;
---
+++ curriculum-main/foundations/javascript_basics/loops_and_arrays/04.js
@@ -0,0 +1,19 @@
+/*
+Sort in decreasing order
+Written By: Yasin Zahir
+-----------------------
+importance: 4
+let arr = [5, 2, 1, -10, 8];
+
+// ... your code to sort it in decreasing order
+
+alert( arr ); // 8, 5, 2, 1, -10
+
+*/
+
+let arr = [5, 2, 1, -10, 8];
+
+arr.sort();
+arr.reverse();
+
+console.log(arr); // 8, 5, 2, 1, -10
