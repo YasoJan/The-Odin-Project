78d5685014fbce78720a0017a20851516299de4d
yasojan <yasinzahir98@gmail.com>
2025-12-14 15:13:48 -0800

    implement removal from array function

--- curriculum-main/foundations/javascript_basics/javascript-exercises-main/foundations/08_removeFromArray/removeFromArray.js
+++ curriculum-main/foundations/javascript_basics/javascript-exercises-main/foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, num) {
+  let new_arr = [];
+  for(let i =0; i<arr.length; i++){
+    if(arr[i] != num){
+      new_arr.push(arr[i]);
+    }
+  }
+  return new_arr;
 };
 
+console.log(removeFromArray([1, 2, 3, 4], 3)); // should remove 3 and return [1,2,4]
 // Do not edit below this line
 module.exports = removeFromArray;
